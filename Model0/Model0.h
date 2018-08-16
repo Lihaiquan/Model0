3623cac5e12d4cf220520876241e4aa8c728affc
李海权 <lihaiquan@lihaiquans-macbook-pro.local>
2018-08-16 22:54:57 +0800

    Initial Commit

---
+++ Model0/Model0.h
@@ -0,0 +1,19 @@
+//
+//  Model0.h
+//  Model0
+//
+//  Created by 李海权 on 2018/8/16.
+//  Copyright © 2018年 李海权. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Model0.
+FOUNDATION_EXPORT double Model0VersionNumber;
+
+//! Project version string for Model0.
+FOUNDATION_EXPORT const unsigned char Model0VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Model0/PublicHeader.h>
+
+
